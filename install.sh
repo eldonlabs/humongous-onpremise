f8bb0c041bc6832bf6f2e58385d7ddf47828baab
Mohamed Kamagate <kamagatos@gmail.com>
2022-07-15 12:21:33 -0700

    Update install script to automatically generate the env file

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 # See https://sipb.mit.edu/doc/safe-shell/
 set -euf -o pipefail
 
+# MAYBE_SUDO will prepend "sudo" to command on linux distributions.
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
   export MAYBE_SUDO="sudo"
 else
@@ -63,30 +64,47 @@ command_present() {
 
 INSTALL_DIRECTORY="humongous"
 
+# Make sure wget is installed.
+if ! command_present wget; then
+  if command_present yum; then
+    $MAYBE_SUDO yum install wget
+  elif command_present yum; then
+    $MAYBE_SUDO apt-get install wget
+  fi
+fi
+
+# Let's make sure we do not override an existing installation of humongous-onpremise.
 if [ -d "$INSTALL_DIRECTORY" ]; then
     error_exit "found a directory called $INSTALL_DIRECTORY. Remove it before proceeding..."
 fi
 
 log_step 'setting up install location...' "$INSTALL_DIRECTORY"
 
+# Download humongous-onpremise repository.
 if ! command_present unzip; then
   log_warn '`unzip` not found!'
-  log_warn 'Attempting to git clone instead'
+  log_warn 'Attempting to git clone instead.'
+
   if command_present git; then
-    log_step 'cloning...'
+    log_step 'Cloning...'
     git clone https://github.com/eldonlabs/humongous-onpremise.git
   elif command_present yum; then
-    log_warn 'You did not have git so installing'
-    sudo yum install git
+    log_warn 'You do not have git; Installing now...'
+    $MAYBE_SUDO yum install git
     git clone https://github.com/eldonlabs/humongous-onpremise.git
+  elif command_present apt-get; then
+    log_warn 'You do not have git; Installing now...'
+    $MAYBE_SUDO apt-get install git
+    git clone https://github.com/eldonlabs/humongous-onpremise.git    
   else
-    error_exit "Please install git or unzip before continuing"
+    error_exit "Please install git or unzip before continuing."
   fi
+
   mv humongous-onpremise "$INSTALL_DIRECTORY"
 else
-  log_step 'downloading...'
-  curl -L -XGET -o main.zip https://github.com/eldonlabs/humongous-onpremise/archive/refs/heads/main.zip
-  log_step 'unpacking...'
+  log_step 'Downloading...'
+  wget https://github.com/eldonlabs/humongous-onpremise/archive/refs/heads/main.zip
+  log_step 'Unpacking...'
   unzip main.zip
   mv humongous-onpremise-main "$INSTALL_DIRECTORY"
 fi
@@ -96,8 +114,8 @@ cd "$INSTALL_DIRECTORY"
 # Install docker.
 if ! command_present docker; then
   log_warn '`docker` not found! Attempting to install. This may take a few minutes.'
-  
-  ./get-docker.sh
+
+  wget -qO- https://get.docker.com/ | sh
 
   if ! command_present docker; then
     if [[ "$OSTYPE" == "darwin"* ]]; then
@@ -112,7 +130,53 @@ if ! command_present docker; then
   fi
 else
   log_step '`docker` found!'
-fi  
+fi
 
+# Before creating the env file, let's make sure the user has a license key.
+echo "Please enter your license key. Contact licensing@humongous.io if you do not have a key."
+read -p "Enter it here: " licenseKey
+
+if [[ "$licenseKey" == "" ]]; then
+  error_exit "Please contact licensing@humongous.io to obtain your license key."
+fi
+
+# Create env file.
+if [ -f ./.env ]; then
+  mv .env .env.$(date +"%Y-%m-%d_%H-%M-%S")
+fi
+touch .env
+
+echo 'NODE_ENV=onpremise' >> .env
+echo '' >> .env
+
+# DB info.
+echo "# Database admin credentials for the app's data and logs." >> .env
+echo "DB_ADMIN_USERNAME=hio" >> .env
+echo "DB_ADMIN_PASSWORD=secret" >> .env
+echo '' >> .env
+
+# Add the current public IP as the base address. Default to localhost.
+if command_present dig; then
+  baseDomain=$(dig -4 TXT +short o-o.myaddr.l.google.com @ns1.google.com | sed -e 's/^"//' -e 's/"$//')
+else
+  baseDomain="localhost"
+fi
+
+echo '# Domain pointing to your Humongous deployment.' >> .env
+echo "BASE_DOMAIN=http://${baseDomain}:8080" >> .env
+echo '' >> .env
+
+# Capture the license key.
+echo '# License key.' >> .env
+echo "LICENSE_KEY=${licenseKey}" >> .env
+echo '' >> .env
+
+# Cookie insecure value.
+echo "# Sends auth requests with insecure cookies." >> .env
+echo "# Set to true if hosting Humongous on a non-HTTPS URL or raw IP address." >> .env
+echo "# This is typically used if you haven't deployed Humongous on a custom domain yet." >> .env
+echo "COOKIE_INSECURE=true" >> .env
+
+# Start the app.
 log_step "Building Humongous!"
 $MAYBE_SUDO docker compose up -d
